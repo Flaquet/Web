7168e66162efe275656dda1e9362b4a42eca8f57
FLAQUET <55395047+Flaquet@users.noreply.github.com>
2019-11-21 10:39:01 +0100

    enfin

---
+++ PHP/exo1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo1</title>
+</head>
+<body>
+    
+    <div>
+
+        je suis un nombre 
+
+    </div>
+
+    <?php
+    $NombreAleatoire = rand(0,100); 
+    if($NombreAleatoire%2 ==1){
+
+        
+
+    }else{
+
+
+
+    }?>
+
+</body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Entrainement PHP</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Serveur DouDou"; ?>
+    </h1>
+    <h2>
+        test 1
+    </h2>
+   
+    <?php $Testvariable="je suis une variable";
+              echo $Testvariable;
+        ?>
+    
+</body>
+</html>
